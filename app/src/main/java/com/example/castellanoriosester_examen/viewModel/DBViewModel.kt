95a4f17b63a23a428c323fd7d5f6c1bdad9ff993
Ester Castellano Ríos <estercastellanorios@gmail.com>
2026-02-11 18:25:34 +0100

    Viewmoddel con los metodos CRUD hecho: falta integrar los datos dinamicaamente en las pantallas

--- app/src/main/java/com/example/castellanoriosester_examen/viewModel/DBViewModel.kt
+++ app/src/main/java/com/example/castellanoriosester_examen/viewModel/DBViewModel.kt
@@ -2,10 +2,89 @@ package com.example.castellanoriosester_examen.viewModel
 
 import androidx.lifecycle.ViewModel
 import com.example.castellanoriosester_examen.modelo.Jugador
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlin.String
+import kotlin.collections.mapNotNull
+import kotlin.collections.set
+import kotlin.jvm.java
+import kotlin.text.isNotBlank
+import kotlin.text.toDouble
 
 class DBViewModel: ViewModel() {
-    private val _juagdores = MutableStateFlow<List<Jugador>>(emptyList())
-    val nombreLista: StateFlow<List<Jugador>> = _juagdores
+    private val db = Firebase.firestore
+    private val jugadoresCollection = db.collection("Jugadores")
+
+    private val _Jugadores = MutableStateFlow<List<Jugador>>(emptyList())
+    val jugadores: StateFlow<List<Jugador>> = _Jugadores
+
+
+    init {
+        getJugadores()
+    }
+
+    private fun getJugadores() {
+        jugadoresCollection.addSnapshotListener { snapshot, error ->
+            if (error != null) {
+                return@addSnapshotListener
+            }
+            if (snapshot != null) {
+                val JugadoresList = snapshot.documents.mapNotNull { doc ->
+                    val jugador = doc.toObject(Jugador::class.java)
+                    jugador?.id = doc.id
+                    jugador
+                }
+                _Jugadores.value = JugadoresList
+            }
+        }
+    }
+
+    fun addJugador(nombre: String, numero: Int, posicion: String, nacionalidad: String, imagen: String) {
+        val Jugador = Jugador(
+            nombre = nombre,
+            numero = numero,
+            posicion = posicion,
+            nacionalidad = nacionalidad,
+            imagen = imagen
+        )
+
+        jugadoresCollection.add(Jugador)
+            .addOnFailureListener {}
+            .addOnSuccessListener { }
+            .addOnCompleteListener { }
+    }
+
+    fun deleteJugador(id: String) {
+        jugadoresCollection.document(id)
+            .delete()
+            .addOnFailureListener {}
+            .addOnFailureListener {}
+    }
+
+    fun updateJugador(id: String, nombre: String, numero: String, posicion: String, nacionalidad: String, imagen: String) {
+        val data = mutableMapOf<String, Any>()
+
+        if (nombre.isNotBlank()) {
+            data["nombre"] = nombre
+        }
+        if (numero.isNotBlank()) {
+            data["numero"] = numero.toDouble()
+        }
+        if (posicion.isNotBlank()) {
+            data["posicion"] = posicion
+        }
+        if (nacionalidad.isNotBlank()) {
+            data["nacionalidad"] = nacionalidad
+        }
+        if (imagen.isNotBlank()) {
+            data["imagen"] = imagen
+        }
+
+        jugadoresCollection.document(id)
+            .update(data)
+            .addOnSuccessListener {}
+            .addOnFailureListener {}
+    }
 }
